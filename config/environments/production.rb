02b502e18a18a858b663cfec4ba12fdd380d375c
railskipl <rails@kunalinfotech.net>
2011-03-14 11:59:15 +0530

    a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
-config.cache_classes = true
-config.action_controller.consider_all_requests_local = false
-config.action_controller.perform_caching = true
-config.action_view.cache_template_loading = true
+config.cache_classes = false
+config.whiny_nils = true
+config.action_controller.consider_all_requests_local = true
+config.action_view.debug_rjs = true
+config.action_controller.perform_caching = false
+config.action_mailer.raise_delivery_errors = false
+config.logger = Logger.new(config.log_path, 2, 20.megabytes)
