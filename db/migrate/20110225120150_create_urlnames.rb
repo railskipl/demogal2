a10b6f42bd03d68897c0b6296bf15803af5908d8
railskipl <rails@kunalinfotech.net>
2011-03-14 12:38:20 +0530

    A

--- db/migrate/20110225120150_create_urlnames.rb
+++ db/migrate/20110225120150_create_urlnames.rb
@@ -2,7 +2,7 @@ class CreateUrlnames < ActiveRecord::Migration
   def self.up
     create_table :urlnames do |t|
       t.string :nameable_type
-      t.string :nameable_id
+      t.integer :nameable_id
       t.string :name
 
       t.timestamps
