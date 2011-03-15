f408e9908df97315f80a37f22931ca7704d842bd
railskipl <rails@kunalinfotech.net>
2011-03-15 14:30:15 +0530

    a

--- app/models/artist_photo.rb
+++ app/models/artist_photo.rb
@@ -11,13 +11,12 @@ class ArtistPhoto < ActiveRecord::Base
    
   has_attached_file :photo, 
                     :styles => { :original => "", :slider =>"", :medium => "", :thumb => "" },
-                    :storage => :s3, :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
-                    :path => "public/attachments/artist-photos/:id/:style/:basename.:extension",
+                    
                     :convert_options => {
                       :original => "",
-                      :slider =>"-gravity center -thumbnail 305x300^ -extent 496x330",
-                      :medium => "-gravity center -thumbnail 305x300^ -extent 255x250",
-                      :thumb => "-gravity center -thumbnail 85x75^ -extent 85x75"
+                      :slider =>"-gravity center -thumbnail 460x300^ -extent 480x330",
+                      :medium => "-gravity center -thumbnail 255x250^ -extent 270x250",
+                      :thumb => "-gravity center -thumbnail 75x55^ -extent 85x75"
                       }
    validates_attachment_presence :photo
    validates_attachment_size :photo, :less_than => 5000000                   
