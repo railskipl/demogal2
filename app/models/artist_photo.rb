53cd8980a0d6f731041c67db5604fbb2a6cf77ef
railskipl <rails@kunalinfotech.net>
2011-03-15 14:31:10 +0530

    changes

--- app/models/artist_photo.rb
+++ app/models/artist_photo.rb
@@ -11,7 +11,8 @@ class ArtistPhoto < ActiveRecord::Base
    
   has_attached_file :photo, 
                     :styles => { :original => "", :slider =>"", :medium => "", :thumb => "" },
-                    
+                    :storage => :s3, :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+                    :path => "public/attachments/artist-photos/:id/:style/:basename.:extension",
                     :convert_options => {
                       :original => "",
                       :slider =>"-gravity center -thumbnail 460x300^ -extent 480x330",
